23f63878d69f599cd125a4dc4f2e73e74cb47b15
Nurjigit <nurart113@gmail.com>
2024-08-06 02:06:52 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    screens: {
+      '2xl': '1200px',
+      xl: '992px',
+      lg: '768px',
+      md: '480px',
+      sm: '320px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
